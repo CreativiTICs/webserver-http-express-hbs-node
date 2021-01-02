8f22fa10fb09420566784f25e3813d9fd63b0bca
CreativiTICs <creativiticscolombia@gmail.com>
2021-01-02 12:03:18 -0500

    Webserver Node JS

---
+++ README.md
@@ -0,0 +1,7 @@
+## Webserver - HTTP - Express - HBS con Node JS
+
+Aplicación creada con Node JS
+
+```
+nodemon server
+```
---
+++ server.js
@@ -0,0 +1,27 @@
+const { text } = require("express");
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+require('./hbs/helpers');
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.get("/", (req, res)=> {
+
+    res.render('home', {
+        nombre: 'carlos'
+    });
+});
+
+app.get("/about", (req, res)=> {
+
+    res.render('about');
+});
+
+app.listen(3000, ()=>{
+    console.log('Server conectado en el puerto 3000');
+});
